abce40499e4455cc3b3530681d011b8d2fdcef4d
Andrew <andrescbc4@gmail.com>
2021-01-08 20:02:04 -0500

    Changed condition of happy result

--- Calculator.py
+++ Calculator.py
@@ -7,11 +7,6 @@ print('#### 2. Divition     ####')
 print('#### 3. Substraction ####')
 print('#### 4. Multiplication ####')
 
-print('#### RANDOM CHANGE ####')
-
-print('#### RANDOM CHANGE 2 ####')
-
-
 operation = int(input('Choose an operation: \n'))
 
 
@@ -21,37 +16,26 @@ if operation == 1:
     print('### ADDITION ###')
     print ('the operation result is: \n')
     result = num_1 + num_2
-    if result == 420 : 
-        print(f'Happy {result}')
-    else: 
-        print (result)
+    print(result)
 elif operation == 2:
     # Clear terminal
     print(chr(27) + "[2J")
     print('### DIVITION ###')
     print ('the operation result is: \n')
     result = num_1 / num_2
-    if result == 420 : 
-        print(f'Happy {result}')
-    else: 
-        print (result)
+    print(result)
+    
 elif operation == 3:
     # Clear terminal
     print(chr(27) + "[2J")
     print('### SUBSTRACTION ###')
     print ('the operation result is: \n')
     result = num_1 - num_2
-    if result == 420 : 
-        print(f'Happy {result}')
-    else: 
-        print (result)
+    print(result)
 elif operation == 4:
     # Clear terminal
     print(chr(27) + "[2J")
     print('### MULTIPLICATION ###')
     print ('the operation result is: \n')
     result = num_1 * num_2
-    if result == 420 : 
-        print(f'Happy {result}')
-    else: 
-        print (result)
+    print(result)
